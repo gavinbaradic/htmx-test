177d7d81150b63a2a5fa464b86fdd6ac03425190
gavinbaradic <studio@gavinbaradic.com>
2024-01-31 10:16:23 -0600

    add alpine & clean up server

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
+const defaultFile = 'index.html'
+const isFile = (pathname: string) => pathname.match(/\.[^\/]+$/)
+
 Bun.serve({
   port: 3000,
   fetch(req) {
-    const url = new URL(req.url)
-    return new Response(
-      Bun.file(import.meta.dir + url.pathname + '/index.html'),
-    )
+    const { pathname } = new URL(req.url)
+    const filePath = isFile(pathname)
+      ? pathname
+      : pathname.endsWith('/')
+      ? `${pathname}${defaultFile}`
+      : `${pathname}/${defaultFile}`
+
+    return new Response(Bun.file(`${import.meta.dir}/src/${filePath}`))
   },
 })
